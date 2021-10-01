677d07f90973a2a6032538c98e6751406e152e41
murtada58 <murtada58@hotmail.com>
2021-10-01 13:08:09 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,151 @@
+const SERVER_IP =  "176.58.109.37" // set to "127.0.0.1" or your servers ip if you want to host your own server
+const PORT = "6789"
+
+let websocket = new WebSocket(`ws://${SERVER_IP}:${PORT}/`);
+
+let board = [["","",""], ["","",""],["","",""]];
+
+const users = document.getElementById("user-count");
+const createGame = document.getElementById("create-game");
+const leave = document.getElementById("leave");
+const lobbies = document.getElementById("lobbies");
+const game = document.getElementById("game");
+const turn = document.getElementById("turn");
+
+for (i=0; i<9; i++)
+{
+    document.getElementById(i).onclick = function () {
+        console.log(this.dataset.num);
+        websocket.send(JSON.stringify({action: 'update', num: this.dataset.num}));
+    }
+}
+createGame.onclick = () => {
+    websocket.send(JSON.stringify({action: 'create'}));
+    lobbies.style.display = "none"
+    game.style.display = "grid"
+    createGame.style.display = "none"
+    leave.style.display = "grid"
+}
+
+leave.onclick = () => {
+    websocket.send(JSON.stringify({action: 'leave'}));
+    lobbies.style.display = "block"
+    game.style.display = "none"
+    createGame.style.display = "grid"
+    leave.style.display = "none"
+}
+
+function addLobby(lobbies, lobbyName, numberOfPlayers)
+{
+    const lobby = document.createElement("div");
+    lobby.classList.add("lobby");
+    lobby.dataset.name = lobbyName;
+
+    const name = document.createElement("p")
+    name.classList.add("lobby-name");
+    name.textContent = `Lobby: ${lobbyName}`
+
+    const users = document.createElement("p")
+    users.classList.add("lobby-users");
+    users.textContent = `Users: ${numberOfPlayers}/2`
+
+    const button = document.createElement("p")
+    button.classList.add("lobby-button");
+    if (numberOfPlayers < 2)
+    {
+        button.classList.add("active")
+        button.textContent = "Join"
+    }
+    else
+    {
+        button.classList.add("inactive")
+        button.textContent = "Full"
+    }
+    button.onclick = () => {
+        if (button.classList.contains("active"))
+        {
+            websocket.send(JSON.stringify({action: 'join', name: lobbyName}));
+        }
+    }
+
+
+    lobby.appendChild(name);
+    lobby.appendChild(users);
+    lobby.appendChild(button);
+
+    lobbies.appendChild(lobby);
+}
+
+function clearLobbies(lobbies)
+{
+    while (lobbies.lastChild) {
+        lobbies.removeChild(lobbies.lastChild);
+    }
+}
+
+websocket.onmessage = function (event) {
+    data = JSON.parse(event.data);
+    switch (data.type) {
+        case 'users':
+            users.textContent = (
+                data.count.toString() + " user" +
+                (data.count == 1 ? "" : "s") + " currently online");
+            break;
+
+        case 'lobby':
+            clearLobbies(lobbies);
+            for (const [key, value] of Object.entries(data.lobbies))
+            {
+                addLobby(lobbies, key, value)
+            }
+            break;
+
+        case 'join':
+            console.log("joined")
+            lobbies.style.display = "none"
+            game.style.display = "grid"
+            createGame.style.display = "none"
+            leave.style.display = "grid"
+            if (data.turn == "you")
+            {
+                turn.textContent = "Your turn"
+            }
+            else
+            {
+                turn.textContent = "Enemy turn"
+            }
+            break;
+            
+        case 'enemy_leave':
+            turn.textContent = "Enemy Left the game wait for someone else to join"
+            break;
+
+        case 'update':
+            for (i=0; i<9; i++)
+            {
+                document.getElementById(i).textContent = data.board[i]
+            }
+
+            if (data.turn == "you")
+            {
+                turn.textContent = "Your turn"
+            }
+            else
+            {
+                turn.textContent = "Enemy turn"
+            }
+
+            if (data.outcome == 'false')
+            {
+                console.log(data.board)
+            }
+            else
+            {
+                turn.textContent = `You ${data.outcome}`
+            }
+
+            break;
+        default:
+            console.error("unsupported event", data);
+    }
+};
---
+++ server.py
@@ -0,0 +1,237 @@
+#!/usr/bin/env python
+
+import asyncio
+import json
+import logging
+import websockets
+
+SERVER_IP =  "176.58.109.37" # set to "localhost" or your servers ip if you want to host your own server
+PORT = 6789
+
+logging.basicConfig()
+
+USERS = set()
+USERS_DATA = {}
+GAMES = {}
+
+new_game_id = 1
+
+def update_event(game_id, user, turn):
+    board = []
+    for cell in GAMES[game_id]["board"]:
+        if cell == "":
+            board.append("")
+        elif cell == user:
+            board.append("X")
+        else:
+            board.append("O")
+
+    if GAMES[game_id]["outcome"] == False:
+        return json.dumps({"type": "update", "board": board, "turn": turn, "outcome": "false"})
+    elif GAMES[game_id]["outcome"] == user:
+        return json.dumps({"type": "update", "board": board, "turn": turn, "outcome": "won"})
+    else:
+        return json.dumps({"type": "update", "board": board, "turn": turn, "outcome": "lost"})
+
+
+def users_event():
+    return json.dumps({"type": "users", "count": len(USERS)})
+
+def game_event():
+    lobbies = {name: len(GAMES[name]["users"]) for name in GAMES}
+    return json.dumps({"type": "lobby", "lobbies": lobbies})
+
+def join_event(turn):
+    return json.dumps({"type": "join", "turn": turn})
+
+async def remove_from_game(websocket):
+    if USERS_DATA[websocket]["game"] != None:
+        if len(GAMES[USERS_DATA[websocket]["game"]]["users"]) < 2: # if only user in the game delete the game
+            del GAMES[USERS_DATA[websocket]["game"]]
+            USERS_DATA[websocket]["game"] = None
+        else: # if another user still in the game keep the game but remove the disconnecting user
+            for user in GAMES[USERS_DATA[websocket]["game"]]["users"]:
+                if user != websocket:
+                    try:
+                        await user.send(json.dumps({"type": "enemy_leave"}))
+                    except:
+                        del GAMES[USERS_DATA[user]["game"]]
+                        USERS_DATA[user]["game"] = None
+            GAMES[USERS_DATA[websocket]["game"]]["users"].remove(websocket)
+            USERS_DATA[websocket]["game"] = None
+    
+
+def validate(user, num):
+    if USERS_DATA[user]["game"] not in GAMES:
+        print("User not in game")
+        return False
+    elif len(GAMES[USERS_DATA[user]["game"]]["users"]) < 2:
+        print("game still pending another player")
+        return False
+    elif GAMES[USERS_DATA[user]["game"]]["turn"] != user:
+        print("not the current users turn")
+        return False
+    elif GAMES[USERS_DATA[user]["game"]]["board"][int(num)] != "":
+        print("cell not empty")
+        return False
+    
+    return True
+
+def won(user, board):
+    # check rows
+    for i in range(3):
+        enemy_count = 0
+        user_count = 0
+        for j in range(3):
+            if board[j + (3*i)] == "":
+                continue
+            elif board[j + (3*i)] == user:
+                user_count += 1
+            else:
+                enemy_count += 1
+
+        if user_count >= 3:
+            return "user"
+        elif enemy_count >= 3:
+            return "enemy"
+    
+    # check columns
+    for i in range(3):
+        enemy_count = 0
+        user_count = 0
+        for j in range(3):
+            if board[i + (3*j)] == "":
+                continue
+            elif board[i + (3*j)] == user:
+                user_count += 1
+            else:
+                enemy_count += 1
+
+        if user_count >= 3:
+            return "user"
+        elif enemy_count >= 3:
+            return "enemy"
+
+    # check right diagonal
+    enemy_count = 0
+    user_count = 0
+    for i in range(3):
+        if board[4*i] == "":
+            continue
+        elif board[4*i] == user:
+            user_count += 1
+        else:
+            enemy_count += 1
+
+    if user_count >= 3:
+        return "user"
+    elif enemy_count >= 3:
+        return "enemy"
+
+    # check left diagonal
+    enemy_count = 0
+    user_count = 0
+    for i in range(1, 4):
+        if board[2*i] == "":
+            continue
+        elif board[2*i] == user:
+            user_count += 1
+        else:
+            enemy_count += 1
+
+    if user_count >= 3:
+        return "user"
+    elif enemy_count >= 3:
+        return "enemy"
+    
+    return False
+
+
+async def game(websocket, path):
+    try:
+        USERS.add(websocket)
+        USERS_DATA[websocket] = {"game": None}
+        websockets.broadcast(USERS, users_event())
+        await websocket.send(game_event())
+
+        global new_game_id
+        async for message in websocket:
+            data = json.loads(message)
+            if data["action"] == "create":
+                if USERS_DATA[websocket]["game"] == None:
+                    GAMES[str(new_game_id)] = {"users": {websocket}, "board": [""]*9, "turn": websocket, "outcome": False}
+                    USERS_DATA[websocket]["game"] = str(new_game_id)
+                    new_game_id += 1
+                    websockets.broadcast(USERS, game_event())
+                    await websocket.send(update_event(USERS_DATA[websocket]["game"], websocket, "you"))  
+                else:
+                    print(f"User tried to create new game when they are already in a game {websocket}")
+
+            elif data["action"] == "join":
+                if data["name"] in GAMES and len(GAMES[data["name"]]["users"]) == 1:
+                    GAMES[data["name"]]["users"].add(websocket)
+                    USERS_DATA[websocket]["game"] = data["name"]
+                    GAMES[USERS_DATA[websocket]["game"]]["outcome"] = False
+                    GAMES[USERS_DATA[websocket]["game"]]["board"] = [""]*9
+                    for user in GAMES[data["name"]]["users"]:
+                        if user == websocket:
+                            await user.send(join_event("enemy"))
+                            await user.send(update_event(USERS_DATA[websocket]["game"], user, "enemy")) 
+                        else:
+                            GAMES[data["name"]]["turn"] = user
+                            await user.send(join_event("you"))
+                            await user.send(update_event(USERS_DATA[websocket]["game"], user, "you")) 
+                     
+                    websockets.broadcast(USERS, game_event())
+                else:
+                    print("user tried to join a game that doesnt exist or is full")
+            
+            elif data["action"] == "leave":
+                await remove_from_game(websocket)
+                websockets.broadcast(USERS, game_event())
+
+            elif data["action"] == "update":
+                if GAMES[USERS_DATA[websocket]["game"]]["outcome"] != False:
+                    GAMES[USERS_DATA[websocket]["game"]]["outcome"] = False
+                    GAMES[USERS_DATA[websocket]["game"]]["board"] = [""]*9
+                    for user in GAMES[USERS_DATA[websocket]["game"]]["users"]:
+                        if user == GAMES[USERS_DATA[websocket]["game"]]["turn"]:
+                            await user.send(update_event(USERS_DATA[websocket]["game"], user, "you"))
+                        else:
+                            await user.send(update_event(USERS_DATA[websocket]["game"], user, "enemy"))
+                
+                elif validate(websocket, data["num"]):
+                    GAMES[USERS_DATA[websocket]["game"]]["board"][int(data["num"])] = websocket
+                    output = won(websocket, GAMES[USERS_DATA[websocket]["game"]]["board"])
+                    if output == "user":
+                        GAMES[USERS_DATA[websocket]["game"]]["outcome"] = websocket
+                    elif output == "enemy":
+                        print("this shouldnt be possible")
+                    else:
+                        GAMES[USERS_DATA[websocket]["game"]]["outcome"] = False
+                    
+                     
+                    
+                    for user in GAMES[USERS_DATA[websocket]["game"]]["users"]:
+                        if user != websocket:
+                            GAMES[USERS_DATA[websocket]["game"]]["turn"] = user
+                            await user.send(update_event(USERS_DATA[websocket]["game"], user, "you"))
+                        else:
+                            await user.send(update_event(USERS_DATA[websocket]["game"], user, "enemy"))
+            else:
+                logging.error("unsupported event: %s", data)
+    finally:
+        USERS.remove(websocket)
+        await remove_from_game(websocket)
+        del USERS_DATA[websocket]
+        websockets.broadcast(USERS, users_event())
+        websockets.broadcast(USERS, game_event())
+
+
+async def main():
+    async with websockets.serve(game, SERVER_IP, PORT):
+        await asyncio.Future()  # run forever
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
